5f352403d424831859ce9e800383c8fa16d87165
kwin <2280182048@qq.com>
2020-12-22 17:39:37 +0800

    20201222:add elasticsearch tests

---
+++ src/main/java/com/kwin/forum/dao/elasticsearch/DiscussPostRepository.java
@@ -0,0 +1,10 @@
+package com.kwin.forum.dao.elasticsearch;
+
+import com.kwin.forum.entity.DiscussPost;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiscussPostRepository extends ElasticsearchRepository<DiscussPost,Integer> {
+
+}
--- src/main/java/com/kwin/forum/entity/DiscussPost.java
+++ src/main/java/com/kwin/forum/entity/DiscussPost.java
@@ -1,19 +1,42 @@
 package com.kwin.forum.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
 import java.util.Date;
 
 /**
  * 帖子实体类
  */
+@Document(indexName = "discusspost",type = "_doc",shards = 6,replicas = 3)
 public class DiscussPost {
+    @Id
     private Integer id;//帖子id(主键）
+
+    @Field(type = FieldType.Integer)
     private Integer userId;//用户id(普通索引)
+
+    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String title;//帖子标题
+
+    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String content;//帖子内容
+
+    @Field(type = FieldType.Integer)
     private Integer type;//帖子类型：0-普通，1-置顶
+
+    @Field(type = FieldType.Integer)
     private Integer status;//贴子状态：0-正常,1-精华,2-拉黑
+
+    @Field(type = FieldType.Date)
     private Date createTime;//创建时间
+
+    @Field(type = FieldType.Integer)
     private Integer commentCount;//评论数量
+
+    @Field(type = FieldType.Double)
     private Double score;//帖子评分
 
     public Integer getId() {
